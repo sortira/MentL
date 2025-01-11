b8386cb01a9ca6a7497c862aaf2002b45882fff0
Prayas Sinha <ralphraout@gmail.com>
2025-01-11 18:08:43 +0530

    Fixed issues with login

--- static/login.js
+++ static/login.js
@@ -9,3 +9,20 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+async function loginReq()
+{
+  const emailVal = document.getElementById("email_login").value;
+  const passVal = document.getElementById("password_login").value;
+  fetch("/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: emailVal,
+      password: passVal,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  });
+  
+}
