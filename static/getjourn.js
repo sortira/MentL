e652169047318bacac202f3aceb1272afaee4d4b
Prayas Sinha <ralphraout@gmail.com>
2025-01-12 13:04:32 +0530

    Fixed issues cause by navbar in getjourn

---
+++ static/getjourn.js
@@ -0,0 +1,14 @@
+function loadHome()
+{
+    window.location.href="/"
+}
+
+function loadDash()
+{
+    window.location.href="/dashboard"
+}
+
+function loadAbout()
+{
+    window.location.href="/aboutus"
+}
